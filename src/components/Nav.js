2eacdf2e5777373cb6e2572bf541e5767955a153
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-04 02:30:08 +0200

    nav.js file added in components folder

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+
+
+function Nav(){
+    return (
+        <nav className='container' >
+        <div>
+          <img  alt='logo' src='/images/brand_logo.png' />
+        </div>
+        <ul>
+          <li href="#" >MENU</li>
+          <li href="#" >LOCATION</li>
+          <li href="#" >ABOUT</li>
+          <li href="#" >CONTACT</li>
+        </ul>
+        <button>logo</button>
+    
+        </nav>
+       
+    )
+};
+export default Nav;
