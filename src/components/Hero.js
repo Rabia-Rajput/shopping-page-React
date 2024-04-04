dc91fc413e43c8dd281de6568e929ed678de2504
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-04 02:30:25 +0200

    Hero.js file added in components folder

---
+++ src/components/Hero.js
@@ -0,0 +1,34 @@
+
+function Hero(){
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>Your Feet Deserve The Best</h1>
+                <p> YOUR FEET DESERVE THE BEST 
+                    AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                    YOUR FEET DESERVE THE BEST AND
+                     WE’RE HERE TO HELP YOU WITH OUR SHOES.
+
+</p>
+   <div className="hero-btn"> 
+   <button className="btn1">Shop Now</button>
+   <button className="btn2">Category</button></div>
+   <div className="brand-icon">
+   <p> Also Available On</p>
+   <img src="/images/amazon.png" alt="amazon-logo" />
+            <img src="/images/flipkart.png" alt="flipkart-logo" />
+
+   </div>
+ 
+   
+            </div>
+            <div>
+            <img src="/images/hero-image.png" alt="hero-image" />
+            </div>
+        </main>
+    )
+
+}
+
+
+export default Hero;
